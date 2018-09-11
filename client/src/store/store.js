f6078ada7b2e92b1df35d642bb652a8208d613ca
clement ng <cgng16@gmail.com>
2018-09-11 10:00:01 -0700

    comment

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -5,8 +5,10 @@ import thunk from "redux-thunk";
 const store = createStore(
   rootReducer,
   compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(
+      thunk
+    ) /*,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()*/
   )
 );
 
